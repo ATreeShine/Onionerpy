f1843591ed5936cd356456b4a3b43b92ab47d82e
DjCodessz <154058748+DjCodessz@users.noreply.github.com>
2023-12-18 13:29:53 +0600

    Update main.py
    It will now be optimized

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from fastapi import FastAPI
 from stem.control import Controller
+from uvicorn import run
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
+def create_hidden_service(controller, port):
+    response = controller.create_ephemeral_hidden_service({80: port}, await_publication=True)
+    print(f"Created host: {response.service_id}.onion")
+
 if __name__ == "__main__":
-    from uvicorn import run
-    run(app, host="127.0.0.1", port=5000)
+    local_port = 5000
+    tor_control_port = 9051
 
-    controller = Controller.from_port(port=9051)
-    controller.authenticate()
+    # Start the FastAPI app
+    run(app, host="127.0.0.1", port=local_port)
 
-    response = controller.create_ephemeral_hidden_service({80: 5000}, await_publication=True)
-    print("Created host: {}.onion".format(response.service_id))
+    # Connect to the Tor control port and authenticate
+    with Controller.from_port(port=tor_control_port) as controller:
+        controller.authenticate()
+
+        # Create the hidden service and print the onion address
+        create_hidden_service(controller, local_port)
